6253b60a446df0c54d9aaf6db052c9010b1c42e3
Brian Zhang <brianzhang12705@gmail.com>
2026-03-20 02:34:00 -0400

    added slider

--- basic_toggle.js
+++ basic_toggle.js
@@ -27,7 +27,7 @@ function startTime() {
 
 //utilizes stopWatch function after click on current_time 
 document.getElementById("current_time").addEventListener("click", stopWatch(0));
-document.getElementById("current_time").addEventListener("click", timer(500));
+document.getElementById("current_time").addEventListener("click", timer(5));
 
 function stopWatch(seconds) {
 
@@ -45,11 +45,13 @@ tLength: timer length
 initialT: initial Time
 */
 function timer(tLength){
-  if(tLength <=0 ) 
+  if(tLength < 0) 
     {
       alert("Timer Finished");
       return;
     }
+
+    //might take out of prod, want to have a way to check it counting down 
   const el = document.getElementById('countdown');
   if (!el) {
     console.error("Element #txt not found");
@@ -59,6 +61,28 @@ function timer(tLength){
 
   setTimeout(timer, 1000, tLength-1);
 }
+
+// Slider number 
+var slider = document.getElementById("minSlider");
+var output = document.getElementById("slideText");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+ 
+// prevent negative inputs 
+function preventNegative(e) {
+  let value = e.target.value;
+
+  // Remove anything that's not a digit
+  value = value.replace(/[^0-9]/g, "");
+
+  e.target.value = value;
+}
+
+document.getElementById("hours").addEventListener("input", preventNegative);
+document.getElementById("minutes").addEventListener("input", preventNegative);
 
 function checkTime(i) {
   return i < 10 ? "0" + i : i;
