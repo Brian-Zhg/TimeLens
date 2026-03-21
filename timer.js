1c3db0943b912a794f7f2beb1dd4de14d89c4fa5
Brian Zhang <brianzhang12705@gmail.com>
2026-03-21 15:25:56 -0400

    added files

---
+++ timer.js
@@ -0,0 +1,6 @@
+const el = document.getElementById('hGlass');
+  if (!el) {
+    console.error("Element #txt not found");
+    return;
+  }
+    el.innerHTML = "https://unpkg.com/@lottiefiles/dotlottie-wc@0.9.3/dist/dotlottie-wc.js";
