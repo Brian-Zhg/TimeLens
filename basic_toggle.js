5121282e802f3cc861c7f65437e2fc7e0c3a4c15
Brian Zhang <brianzhang12705@gmail.com>
2026-03-21 04:56:39 -0400

    added new tab

--- basic_toggle.js
+++ basic_toggle.js
@@ -1,8 +1,21 @@
+/*
+Current issue: need to figure out how to get the timer to be remembered and not just cancelled
+after joining 2 or 3 times thinking maybe redo timer to have it based off of the world time 
+*/
+
+src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js">
 document.addEventListener("DOMContentLoaded", () => {
   startTime();
+  chrome.storage.local.get(["time"], (data)=> {
+    if(data.time){
+      timer(data.time);
+      time = data.time;
+    }
+  })
 });
 
 var initialTs = Date.now();
+var sliderNum=50, minutes=0, hours=0, time=0;
 
 function startTime() {
   const today = new Date();
@@ -26,8 +39,7 @@ function startTime() {
 }
 
 //utilizes stopWatch function after click on current_time 
-document.getElementById("current_time").addEventListener("click", stopWatch(0));
-document.getElementById("current_time").addEventListener("click", timer(5));
+document.getElementById("current_time").addEventListener("click", function() {stopWatch(0)});
 
 function stopWatch(seconds) {
 
@@ -40,6 +52,34 @@ function stopWatch(seconds) {
     setTimeout(stopWatch, 1000, seconds+1);
 }
 
+document.getElementById("start").addEventListener("click", function() {getTime();});
+
+async function getTime()
+{
+  let hTime = document.getElementById("hours").value;
+  if(hTime != null && hTime != "") hours = hTime;
+  
+  let mTime = document.getElementById("minutes").value;
+  if(mTime != null && mTime != "") minutes = mTime;
+
+  if(hours > 0 || minutes >0){
+    timer(hours*3600 + minutes*60);
+    time = hours*3600+minutes*60;
+  }
+  else 
+  {
+    timer(sliderNum*60);
+    time = sliderNum*60;
+  }
+  const content = document.getElementById('content');
+
+  // Fetch the HTML of the other file
+  const response = await fetch(chrome.runtime.getURL('timer.html'));
+  const html = await response.text();
+
+  // Replace current popup content
+  content.innerHTML = html;
+}
 /*
 tLength: timer length 
 initialT: initial Time
@@ -58,7 +98,9 @@ function timer(tLength){
     return;
   }
   el.innerHTML = tLength; 
-
+  time--;
+  console.log(time);
+  chrome.storage.local.set({"time": time});
   setTimeout(timer, 1000, tLength-1);
 }
 
@@ -68,7 +110,8 @@ var output = document.getElementById("slideText");
 output.innerHTML = slider.value;
 
 slider.oninput = function() {
-  output.innerHTML = this.value;
+  sliderNum = this.value;
+  output.innerHTML = sliderNum;
 }
  
 // prevent negative inputs 
