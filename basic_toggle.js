a8aab2187c99772101d520c8382e0bac73e69e29
Brian Zhang <brianzhang12705@gmail.com>
2026-03-21 22:39:23 -0400

    Added another html

--- basic_toggle.js
+++ basic_toggle.js
@@ -3,19 +3,20 @@ Current issue: need to figure out how to get the timer to be remembered and not 
 after joining 2 or 3 times thinking maybe redo timer to have it based off of the world time 
 */
 
-src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js">
-document.addEventListener("DOMContentLoaded", () => {
-  startTime();
-  chrome.storage.local.get(["time"], (data)=> {
-    if(data.time){
-      timer(data.time);
-      time = data.time;
-    }
-  })
-});
+src = "https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js" >
+  document.addEventListener("DOMContentLoaded", () => {
+    startTime();
+    chrome.storage.local.get(["time"], (data) => {
+      if (data.time > 0) {
+        console.log("time:" +time + "\n");
+        time = data.time;
+        change();
+      }
+    })
+  });
 
 var initialTs = Date.now();
-var sliderNum=50, minutes=0, hours=0, time=0;
+var sliderNum = 50, minutes = 0, hours = 0, time = 0;
 
 function startTime() {
   const today = new Date();
@@ -24,7 +25,7 @@ function startTime() {
   let s = checkTime(today.getSeconds());
   //part of the day pm vs am 
   let p;
-  if(h > 12) p = "PM";
+  if (h > 12) p = "PM";
   else p = "AM";
 
   const el = document.getElementById('txt');
@@ -33,13 +34,13 @@ function startTime() {
     return;
   }
 
-    el.innerHTML = Math.abs(h-12) + ":" + m + ":" + s + " " + p;
-  
+  el.innerHTML = Math.abs(h - 12) + ":" + m + ":" + s + " " + p;
+
   setTimeout(startTime, 1000);
 }
 
 //utilizes stopWatch function after click on current_time 
-document.getElementById("current_time").addEventListener("click", function() {stopWatch(0)});
+document.getElementById("current_time").addEventListener("click", function () { stopWatch(0) });
 
 function stopWatch(seconds) {
 
@@ -48,29 +49,31 @@ function stopWatch(seconds) {
     console.error("Element #txt not found");
     return;
   }
-    el.innerHTML = seconds;
-    setTimeout(stopWatch, 1000, seconds+1);
+  el.innerHTML = seconds;
+  setTimeout(stopWatch, 1000, seconds + 1);
 }
 
-document.getElementById("start").addEventListener("click", function() {getTime();});
+document.getElementById("start").addEventListener("click", function () { getTime(); });
 
-async function getTime()
-{
+async function getTime() {
   let hTime = document.getElementById("hours").value;
-  if(hTime != null && hTime != "") hours = hTime;
-  
-  let mTime = document.getElementById("minutes").value;
-  if(mTime != null && mTime != "") minutes = mTime;
+  if (hTime != null && hTime != "") hours = hTime;
 
-  if(hours > 0 || minutes >0){
-    timer(hours*3600 + minutes*60);
-    time = hours*3600+minutes*60;
+  let mTime = document.getElementById("minutes").value;
+  if (mTime != null && mTime != "") minutes = mTime;
+
+  if (hours > 0 || minutes > 0) {
+    time = hours * 3600 + minutes * 60;
   }
-  else 
-  {
-    timer(sliderNum*60);
-    time = sliderNum*60;
+  else {
+    time = sliderNum * 60;
   }
+  chrome.storage.local.set({ "time": time });
+  change();
+}
+
+//changes to timer html
+async function change(){
   const content = document.getElementById('content');
 
   // Fetch the HTML of the other file
@@ -79,41 +82,24 @@ async function getTime()
 
   // Replace current popup content
   content.innerHTML = html;
-}
-/*
-tLength: timer length 
-initialT: initial Time
-*/
-function timer(tLength){
-  if(tLength < 0) 
-    {
-      alert("Timer Finished");
-      return;
-    }
 
-    //might take out of prod, want to have a way to check it counting down 
-  const el = document.getElementById('countdown');
-  if (!el) {
-    console.error("Element #txt not found");
-    return;
-  }
-  el.innerHTML = tLength; 
-  time--;
-  console.log(time);
-  chrome.storage.local.set({"time": time});
-  setTimeout(timer, 1000, tLength-1);
-}
+  //loads js file
+  const script = document.createElement("script");
+  script.src = chrome.runtime.getURL("timer.js");
+  script.type = "text/javascript";
 
+  document.body.appendChild(script);
+}
 // Slider number 
 var slider = document.getElementById("minSlider");
 var output = document.getElementById("slideText");
 output.innerHTML = slider.value;
 
-slider.oninput = function() {
+slider.oninput = function () {
   sliderNum = this.value;
   output.innerHTML = sliderNum;
 }
- 
+
 // prevent negative inputs 
 function preventNegative(e) {
   let value = e.target.value;
--- timer.js
+++ timer.js
@@ -1,6 +1,53 @@
-const el = document.getElementById('hGlass');
-  if (!el) {
-    console.error("Element #txt not found");
-    return;
-  }
-    el.innerHTML = "https://unpkg.com/@lottiefiles/dotlottie-wc@0.9.3/dist/dotlottie-wc.js";
+chrome.storage.local.get(["time"], (data)=> {
+    if(data.time > 0){
+      time = data.time;
+    }
+  })
+  timeRemain();
+
+function timeRemain()
+{
+    if(time < 0 ){
+        alert("Timer Finished");
+        change();
+        return;
+    } 
+  let e = document.getElementById('timeRemain');
+  if(!e) 
+    {
+        console.log("i cant find time");
+        return; 
+    }
+  let d = Math.floor(time/(86400));
+  let h = Math.floor((time%86400)/3600);
+  let m = Math.floor((time%3600)/60);
+  let s = Math.floor(time%60);
+  let tString = "";
+  if(d > 0) tString += (d + " Days ");
+  if(h > 0) tString += (h + " Hours ");
+  if(m > 0) tString += (m + " Minutes ");
+  tString += (s + " Seconds");
+  e.innerHTML=tString;
+
+  time--;
+  chrome.storage.local.set({"time": time});
+  setTimeout(timeRemain, 1000);
+}
+
+async function change(){
+  const content = document.getElementById('content');
+
+  // Fetch the HTML of the other file
+  const response = await fetch(chrome.runtime.getURL('basic_toggle.html'));
+  const html = await response.text();
+
+  // Replace current popup content
+  content.innerHTML = html;
+
+  //loads js file
+  const script = document.createElement("script");
+  script.src = chrome.runtime.getURL("basic_toggle.js");
+  script.type = "text/javascript";
+
+  document.body.appendChild(script);
+}
