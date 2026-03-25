a22b4c697889f5037979b791ce1ad8ea9ab0e906
Brian Zhang <brianzhang12705@gmail.com>
2026-03-25 00:45:16 -0400

    still need to work on website inputs

--- blocked.js
+++ blocked.js
@@ -1,6 +1,13 @@
 document.getElementById("addWebsite").addEventListener("click", function () { addWeb() })
-
 let listOfSites = [];
+
+chrome.storage.local.get(["blockedSites"], (data) => {
+    let listOfSites = data.blockedSites ?? [];
+    
+    // do everything that needs listOfSites in here
+    console.log(listOfSites);
+});
+
 function addWeb() {
     let el = document.getElementById("websiteInput").value;
     if (el != null && el != "") {
@@ -14,10 +21,19 @@ async function displayList() {
     let result = await chrome.storage.local.get("blockedSites");
     let list = result.blockedSites || [];
     list.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
-    let textList = document.getElementById("listContainer")
     for (let i = 0; i < list.length; i++) {
         var entry = document.createElement('li');
         entry.appendChild(document.createTextNode(list[i]));
         textList.appendChild(entry);
+    }
+}
+
+document.getElementById("clear").addEventListener("click", function () { clearList() });
+//clears list of blocked websites
+function clearList() {
+    let conformation = confirm("Are you sure you want to clear the list ? ");
+    if(conformation){
+        chrome.storage.local.set({ "blockedSites":[] });
+        displayList();
     }
 }
